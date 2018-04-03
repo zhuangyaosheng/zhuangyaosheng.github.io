5b27e67baadd70c76965ee9a2a58902d54c16cdc
zhuangyaosheng <13570930240@163.com>
2018-04-03 15:37:34 +0800

    增加dist

---
+++ dist/service-worker.js
@@ -0,0 +1,68 @@
+importScripts('./static/js/workbox-sw.prod.v2.1.3.js');/**
+ * @file service-worker.js with workbox api
+ * @desc [example](https://workbox-samples.glitch.me/examples/workbox-sw/)
+ * @author zhuangyaosheng(13570930240@163.com)
+ */
+
+/* globals WorkboxSW */
+
+const workboxSW = new WorkboxSW({
+    cacheId: 'lavas-cache',
+    ignoreUrlParametersMatching: [/^utm_/],
+    skipWaiting: true,
+    clientsClaim: true
+});
+
+// Define precache injection point.
+workboxSW.precache([
+  {
+    "url": "index.html",
+    "revision": "418f75a4a1c361fc21749a192851f4b7"
+  },
+  {
+    "url": "static/css/index.abbfea17.css"
+  },
+  {
+    "url": "static/js/index.21cd2048.js"
+  },
+  {
+    "url": "static/js/manifest.0a093f3d.js"
+  },
+  {
+    "url": "static/js/vendor.ff4e3f1c.js"
+  },
+  {
+    "url": "static/js/vue.544dbf1d.js"
+  },
+  {
+    "url": "static/js/workbox-sw.prod.v2.1.3.js",
+    "revision": "a9890beda9e5f17e4c68f42324217941"
+  }
+]);
+workboxSW.router.registerNavigationRoute('/index.html');
+
+
+/**
+ * example runningCache with api
+ */
+// workboxSW.router.registerRoute(/^https:\/\/lavas\.baidu\.com\/some\/api/,
+//     workboxSW.strategies.networkFirst());
+
+
+/**
+ * example runningCache with resources from CDN
+ * including maxAge, maxEntries
+ * cacheableResponse is important for CDN
+ */
+// workboxSW.router.registerRoute(/^https:\/\/cdn\.baidu\.com/i,
+//     workboxSW.strategies.cacheFirst({
+//         cacheName: 'lavas-cache-images',
+//         cacheExpiration: {
+//             maxEntries: 100,
+//             maxAgeSeconds: 7 * 24 * 60 * 60
+//         },
+//         cacheableResponse: {
+//             statuses: [0, 200]
+//         }
+//     })
+// );
